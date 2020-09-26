7ec1acb86662bf01d2ef3b1f6a0959a8bd0596df
Alex Bates <alexbates828@gmail.com>
2020-09-26 12:50:50 -0500

    Fix bad test names.

--- src/test_prime_utils.py
+++ src/test_prime_utils.py
@@ -44,55 +44,60 @@ class TestPrimeUtils(unittest.TestCase):
         result = prime_utils.is_prime(n)
         self.assertFalse(result)
 
-    def test_is_prime_returns_false_for_zero(self):
+    def test_is_prime_with_range_by_with_mod_returns_false_for_zero(self):
         n = 0
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertFalse(result)
 
-    def test_is_prime_returns_false_for_one(self):
+    def test_is_prime_with_range_by_with_mod_returns_false_for_one(self):
         n = 1
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertFalse(result)
 
-    def test_is_prime_returns_true_for_two(self):
+    def test_is_prime_with_range_by_with_mod_returns_true_for_two(self):
         n = 2
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertTrue(result)
 
-    def test_is_prime_returns_true_for_three(self):
+    def test_is_prime_with_range_by_with_mod_returns_true_for_three(self):
         n = 3
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertTrue(result)
 
-    def test_is_prime_returns_false_for_four(self):
+    def test_is_prime_with_range_by_with_mod_returns_false_for_four(self):
         n = 4
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertFalse(result)
 
-    def test_is_prime_returns_false_for_composite(self):
+    def test_is_prime_with_range_by_with_mod_returns_false_for_composite(self):
         n = 169
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertFalse(result)
 
-    def test_is_prime_returns_false_for_second_composite(self):
+    def test_is_prime_with_range_by_with_mod_returns_false_for_second_composite(self):
         n = 144
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertFalse(result)
 
-    def test_is_prime_returns_true_for_prime_number(self):
+    def test_is_prime_with_range_by_with_mod_returns_true_for_prime_number(self):
         n = 101
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertTrue(result)
 
-    def test_is_prime_returns_true_for_negative_prime(self):
+    def test_is_prime_with_range_by_with_mod_returns_true_for_negative_prime(self):
         n = -101
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertTrue(result)
 
-    def test_is_prime_returns_false_for_negative_composite(self):
+    def test_is_prime_with_range_by_with_mod_returns_false_for_negative_composite(self):
         n = -1234321 #  1234321 = 1111 * 1111
         result = prime_utils.is_prime_with_range_by_with_mod(n)
         self.assertFalse(result)
+
+    def test_is_prime_with_range_by_mod_returns_true_for_large_prime(self):
+        n = 23215657731638
+        result = prime_utils.is_prime_with_range_by_with_mod(n, 1)
+        self.assertTrue(result)
 
     def test_range_by_with_mod_returns_all_nums_for_relatively_prime(self):
         n = 101
