8a31041bb1124e0dd2b2c048dff744f010099d1a
Alex Bates <alexbates828@gmail.com>
2020-09-26 11:55:39 -0500

    Add range_by_with_mod generator and a new function for checking primality. Updated unit tests.

--- src/prime_utils.py
+++ src/prime_utils.py
@@ -1,4 +1,4 @@
-from math import ceil, sqrt
+from math import ceil, sqrt, gcd
 
 def is_prime(n: int) -> bool:
     if type(n)==float:
@@ -11,4 +11,48 @@ def is_prime(n: int) -> bool:
     for i in range(3, sqrtn+1, 2):
         if n%i == 0:
             return False
+    return True
+
+def range_by_with_mod(n: int, b: int):
+    if n==0:
+        pass
+    if n==1:
+        yield 0
+        pass
+    if n < 0 or b <= 0:
+        raise ValueError("Both arguments must be greater than zero!")
+    if b > n:
+        return range_by_with_mod(n, b%n)
+    assert gcd(n,b)==1, f"The greatest common divisor of n and b must be 1."
+
+    iters = 0
+    r = 0
+    while True:
+        yield r
+        r = (r + b)%n
+        if r==0:
+            break
+        iters+=1
+        if iters > n:
+            raise RuntimeException(f"While loop should not execute more than input {n} number of times! Infinite loop prevented.")
+    pass
+
+def is_prime_with_range_by_with_mod(n: int, b: int = None) -> bool:
+    if type(n)==float:
+        raise TypeError("Input parameter n should be of type 'int'.")
+    if n<0:
+        return is_prime(-n)
+    if n in [0, 1]:
+        return False
+    if n==2:
+        return True
+    sqrtn = b or ceil(sqrt(n))
+    try:
+        for i in range_by_with_mod(n, sqrtn):
+            if i<=1:
+                continue
+            if n%i == 0:
+                return False
+    except AssertionError:
+        return False
     return True
--- src/test_prime_utils.py
+++ src/test_prime_utils.py
@@ -44,6 +44,70 @@ class TestPrimeUtils(unittest.TestCase):
         result = prime_utils.is_prime(n)
         self.assertFalse(result)
 
+    def test_is_prime_returns_false_for_zero(self):
+        n = 0
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertFalse(result)
+
+    def test_is_prime_returns_false_for_one(self):
+        n = 1
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertFalse(result)
+
+    def test_is_prime_returns_true_for_two(self):
+        n = 2
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertTrue(result)
+
+    def test_is_prime_returns_true_for_three(self):
+        n = 3
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertTrue(result)
+
+    def test_is_prime_returns_false_for_four(self):
+        n = 4
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertFalse(result)
+
+    def test_is_prime_returns_false_for_composite(self):
+        n = 169
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertFalse(result)
+
+    def test_is_prime_returns_false_for_second_composite(self):
+        n = 144
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertFalse(result)
+
+    def test_is_prime_returns_true_for_prime_number(self):
+        n = 101
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertTrue(result)
+
+    def test_is_prime_returns_true_for_negative_prime(self):
+        n = -101
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertTrue(result)
+
+    def test_is_prime_returns_false_for_negative_composite(self):
+        n = -1234321 #  1234321 = 1111 * 1111
+        result = prime_utils.is_prime_with_range_by_with_mod(n)
+        self.assertFalse(result)
+
+    def test_range_by_with_mod_returns_all_nums_for_relatively_prime(self):
+        n = 101
+        b = 45
+        gen = prime_utils.range_by_with_mod(n,b)
+        l = list(gen)
+        self.assertEqual(n, len(l))
+
+    def test_range_by_with_mod_returns_zero_list_when_n_is_one(self):
+        n = 1
+        b = 45
+        gen = prime_utils.range_by_with_mod(n,b)
+        l = list(gen)
+        self.assertEqual([0], l)
+
 
 if __name__ == '__main__':
     unittest.main()
